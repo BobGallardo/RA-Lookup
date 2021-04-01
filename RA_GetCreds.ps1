d22f1ac8ba8330df501e4c70cde6d818d391653b
BusterBrawls <BusterBrawls@users.noreply.github.com>
2021-03-31 20:59:37 -0700

    added local script path variable

--- RA_GetCreds.ps1
+++ RA_GetCreds.ps1
@@ -1,5 +1,5 @@
-$RAcredsCheck = ".\RA_creds.xml"
+$RAcredsCheck = "$PSScriptRoot\RA_creds.xml"
 
 while (-not(Test-Path -Path $RAcredsCheck -PathType Leaf)) {
-    Get-Credential -Message "Enter your RetroAchievements username and the API key for the password." | Export-Clixml -Path .\RA_creds.xml
+    Get-Credential -Message "Enter your RetroAchievements username and the API key for the password." | Export-Clixml -Path $PSScriptRoot\RA_creds.xml
 }
